1d79d26cc3de6ed6e11caee373c6748b1e0e0862
Vinícius <vncastanheira@gmail.com>
2016-10-21 15:15:08 -0200

    So much aesthetics

---
+++ Assets/Projectiles/LaserController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserController : MonoBehaviour
+{
+	public float Force;
+	public float Lifetime;
+	Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.AddForce(Vector3.forward * Force);
+
+		Destroy(gameObject, Lifetime);
+	}
+	
+}
---
+++ Assets/Scripts/RetroshipController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RetroshipController : MonoBehaviour
+{
+	public float Speed;
+	public Rigidbody Projectile;
+	public Collider collider;
+
+	Ray _ray;
+	void Start () {
+	
+	}
+	
+	void Update ()
+	{
+		var direction = CrossPlatformInputManager.GetAxis("Vertical");
+		transform.Translate(Vector3.forward * direction * Speed);
+
+		_ray = new Ray(transform.position, Vector3.down);
+		RaycastHit hit;
+		if (Physics.Raycast(_ray, out hit))
+		{
+			transform.position = new Vector3(transform.position.x, hit.point.y + 1.0f, transform.position.z);
+		}
+
+		// Shooting
+		if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+		{
+			Instantiate(Projectile, transform.position, Projectile.transform.rotation);
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(_ray);
+	}
+}
