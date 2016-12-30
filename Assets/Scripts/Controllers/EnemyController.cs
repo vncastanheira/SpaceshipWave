744170f0dc27d4e79ae3e9fd683e8cd1986139a6
Vinicius Epiplon <Vinicius Epiplon>
2016-12-30 15:29:15 -0200

    Hello darkness my old friend

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class EnemyController : MonoBehaviour
+{
+	ProjectileLauncher launcher;
+
+    public ParticleSystem DeathEffect;
+    public UnityEvent OnHit;
+    public UnityEvent OnDeath;
+    
+    void Awake()
+	{
+		launcher = GetComponent<ProjectileLauncher>();
+    }
+
+    void Update()
+	{
+		launcher.Launch(Vector3.forward * -1);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            print("OnDeath invoked!");
+            OnDeath.Invoke();
+            Instantiate(DeathEffect, transform.position, transform.rotation);
+        }
+    }
+}
